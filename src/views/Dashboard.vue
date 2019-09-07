28dfb342ac52b83b324fe1bc7b2468664c8b7cad
Kappalucky <kappalucky@aol.com>
2019-09-07 04:51:32 -0400

    fixed error for vue-calendar. shows now. need to add Modal to add sex object

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -1,115 +1,118 @@
 <template>
-  <div>
-    <h1 class="dashboard-heading">Overview</h1>
-    <hr />
+  <div class="dashboard">
+    <section class="dashboard-heading">
+      <h1 class="dashboard-heading-title">Overview</h1>
+      <hr />
+    </section>
+    <section class="container is-widescreen dashboard-body">
 
-    <!-- Section cards -->
-    <section class="overview">
-      <div class="container is-widescreen">
-        <div class="columns is-mobile">
-          <div class="column">
-            <div class="card">
-              <span class="overview-number">{{partners.length}}</span>
-              <p class="overview-text">Partners</p>
+      <!-- Section cards -->
+      <section class="overview">
+          <div class="columns is-mobile">
+            <div class="column">
+              <div class="card">
+                <span class="overview-number">{{partners.length}}</span>
+                <p class="overview-text">Partners</p>
+              </div>
+            </div>
+            <div class="column">
+              <div class="card">
+                <span class="overview-number">{{sexAmount}}</span>
+                <p class="overview-text">times had sex</p>
+              </div>
             </div>
           </div>
-          <div class="column">
-            <div class="card">
-              <span class="overview-number">{{sexAmount}}</span>
-              <p class="overview-text">times had sex</p>
-            </div>
+      </section>
+      <!-- End Section Cards -->
+
+      <!-- Partners Datatable -->
+      <section class="partners">
+        <h1>List of Partners</h1>
+        <b-field grouped group-multiline>
+          <!-- TO-DO: Need to make selection options dynamic based on partner array length -->
+          <b-select v-model="perPage">
+            <option value="5">5 per page</option>
+            <option value="10">10 per page</option>
+            <option value="15">15 per page</option>
+            <option value="20">20 per page</option>
+          </b-select>
+          <div class="control add-button">
+            <button class="button is-primary" @click="isModalActive = true">Add Partner</button>
           </div>
-        </div>
-      </div>
+        </b-field>
+
+        <b-table
+          :data="partners"
+          :paginated="isPaginated"
+          :per-page="perPage"
+          :current-page.sync="currentPage"
+          pagination-position="bottom"
+          default-sort-direction="desc"
+          sort-icon="chevron-up"
+          sort-icon-size="is-small"
+          default-sort="user.first_name"
+          aria-next-label="Next page"
+          aria-previous-label="Previous page"
+          aria-page-label="Page"
+          aria-current-label="Current page"
+        >
+          <template slot-scope="props">
+            <b-table-column
+              field="partners.name"
+              label="Name/Nickname"
+              sortable
+            >{{ props.row.name }}</b-table-column>
+
+            <b-table-column label="Gender"
+            centered>
+              <span>
+                <b-icon pack="fas" :icon="partners.gender === 'Male' ? 'fa-mars' : 'fa-venus'"></b-icon>
+                {{ props.row.gender }}
+              </span>
+            </b-table-column>
+
+            <b-table-column
+              field="user.last_name"
+              label="Location"
+              sortable
+              centered
+            >{{ props.row.location }}</b-table-column>
+
+            <b-table-column field="seconds" label="Date Met" sortable centered>
+              <span class="tag is-success">{{ formatDate(props.row.approxDateMet.seconds) }}</span>
+            </b-table-column>
+
+            <b-table-column centered>
+              <span class="partners-table-buttons">
+                <button class="button is-primary" @click="partnerEdit(props.row.id)">Edit</button>
+                <button class="button is-danger" @click="partnerDelete(props.row.id)">Delete</button>
+              </span>
+            </b-table-column>
+          </template>
+        </b-table>
+      </section>
+      <!-- End Partners Datatable -->
+
+      <!-- Add Partner Modal -->
+      <section class="partner-modal-add">
+        <add-partner-modal />
+      </section>
+      <!-- End Partner Modal -->
+
+      <!-- Edit Partner Modal -->
+      <section class="partner-modal-edit">
+        <edit-partner-modal />
+      </section>
+      <!-- End Edit Partner Modal -->
+
+      <!-- Calendar Component -->
+      <section class="calendar">
+          <vue-calendar :show-limit="3" @day-clicked="dayClicked" @event-clicked="eventClicked">
+          </vue-calendar>
+      </section>
+      <br>
+      <!-- End Calendar Component -->
     </section>
-    <!-- End Section Cards -->
-
-    <!-- Partners Datatable -->
-    <section class="partners">
-      <h1>List of Partners</h1>
-      <b-field grouped group-multiline>
-        <!-- TO-DO: Need to make selection options dynamic based on partner array length -->
-        <b-select v-model="perPage">
-          <option value="5">5 per page</option>
-          <option value="10">10 per page</option>
-          <option value="15">15 per page</option>
-          <option value="20">20 per page</option>
-        </b-select>
-        <div class="control add-button">
-          <button class="button is-primary" @click="isModalActive = true">Add Partner</button>
-        </div>
-      </b-field>
-
-      <b-table
-        :data="partners"
-        :paginated="isPaginated"
-        :per-page="perPage"
-        :current-page.sync="currentPage"
-        pagination-position="bottom"
-        default-sort-direction="desc"
-        sort-icon="chevron-up"
-        sort-icon-size="is-small"
-        default-sort="user.first_name"
-        aria-next-label="Next page"
-        aria-previous-label="Previous page"
-        aria-page-label="Page"
-        aria-current-label="Current page"
-      >
-        <template slot-scope="props">
-          <b-table-column
-            field="partners.name"
-            label="Name/Nickname"
-            sortable
-          >{{ props.row.name }}</b-table-column>
-
-          <b-table-column label="Gender"
-          centered>
-            <span>
-              <b-icon pack="fas" :icon="partners.gender === 'Male' ? 'fa-mars' : 'fa-venus'"></b-icon>
-              {{ props.row.gender }}
-            </span>
-          </b-table-column>
-
-          <b-table-column
-            field="user.last_name"
-            label="Location"
-            sortable
-            centered
-          >{{ props.row.location }}</b-table-column>
-
-          <b-table-column field="seconds" label="Date Met" sortable centered>
-            <span class="tag is-success">{{ formatDate(props.row.approxDateMet.seconds) }}</span>
-          </b-table-column>
-
-          <b-table-column centered>
-            <span class="partners-table-buttons">
-              <button class="button is-primary" @click="partnerEdit(props.row.id)">Edit</button>
-              <button class="button is-danger" @click="partnerDelete(props.row.id)">Delete</button>
-            </span>
-          </b-table-column>
-        </template>
-      </b-table>
-    </section>
-    <!-- End Partners Datatable -->
-
-    <!-- Add Partner Modal -->
-    <section class="partner-modal-add">
-      <add-partner-modal />
-    </section>
-    <!-- End Partner Modal -->
-
-    <!-- Edit Partner Modal -->
-    <section class="partner-modal-edit">
-      <edit-partner-modal />
-    </section>
-    <!-- End Edit Partner Modal -->
-
-    <!-- Calendar Component -->
-    <!--<section class="calendar">
-      <vue-calendar :show-limit="3" @day-clicked="dayClicked" @event-clicked="eventClicked">
-      </vue-calendar>
-    </section>-->
-    <!-- End Calendar Component -->
   </div>
 </template>
 <script>
@@ -177,16 +180,17 @@ export default {
       let unix = moment.unix(date);
       return moment(unix).format("MMM YYYY");
     },
-    /*dayClicked(day) {
+    dayClicked(day) {
       console.log(day);
+      // day.date == date to log for sex object
     },
     eventClicked(event) {
       console.log(event);
-    },*/
+    },
   },
   created() {
-    //this.$calendar.eventBus.$on('day-clicked', day => dayClicked(day));
-    //this.$calendar.eventBus.$on('event-clicked', event => eventClicked(event));
+    this.$calendar.eventBus.$on('day-clicked', day => dayClicked(day));
+    this.$calendar.eventBus.$on('event-clicked', event => eventClicked(event));
   }
 };
 </script>
